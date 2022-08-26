95ba99575d37f844848ffefc4d5df9da16c8e0ee
AnupShrestha28 <anupstha0168@gmail.com>
2022-08-26 21:46:04 +0545

    Intial Commit

--- PHP-Lab/q5.php
+++ PHP-Lab/q5.php
@@ -22,7 +22,7 @@
 <body>
     <?php
     for ($i = 8; $i >= 1; $i--) {
-        for ($j = 1; $j <= $i; ++$j) {
+        for ($j = 1; $j <= $i; $j++) {
             echo "$j";
         }
         echo "<br>";
