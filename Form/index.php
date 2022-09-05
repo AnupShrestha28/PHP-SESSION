9b6529996b1321686c7df31ad6f5b1244911d6da
AnupShrestha28 <anupstha0168@gmail.com>
2022-09-05 23:25:54 +0545

    Initial Commit

---
+++ Form/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post">
+        <input type="text" name="name" placeholder="Enter your name">
+        <input type="email" name="email" placeholder="Enter your email">
+        <input type="password" name="password" placeholder="Enter your password">
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    print_r($_POST);
+    echo "<br>";
+    echo $_POST["name"];
+    echo "<br>";
+    echo $_POST["email"];
+    echo "<br>";
+    echo $_POST["password"];
+    ?>
+</body>
+
+</html>
