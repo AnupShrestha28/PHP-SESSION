0b015330e240302b0d7872119cf257456f847d0e
AnupShrestha28 <anupstha0168@gmail.com>
2022-09-11 10:26:05 +0545

    Initial Commit

---
+++ login/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+
+<body>
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+        <input type="email" name="email" placeholder="Enter your email" required>
+        <input type="password" name="password" placeholder="Enter your password" required>
+        <button>Submit</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        print_r($_POST);
+        echo "<br>";
+        echo $_POST["email"];
+        echo "<br>";
+        echo $_POST["password"];
+    }
+
+
+
+    ?>
+
+</body>
+
+</html>
